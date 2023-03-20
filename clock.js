a8fabacd22dca4f88ce09f266d36e3d29722af0b
Fantom716 <Fantom5431@gmail.com>
2023-03-20 04:42:08 +0400

    JS: Main logic clock and timer

---
+++ clock.js
@@ -0,0 +1,47 @@
+hour = document.querySelector(".hour");
+min = document.querySelector(".min");
+sec = document.querySelector(".seconds");
+timerButton = document.querySelector('.start-timer');
+timerInput = document.querySelector(".timer-time");
+timerInputValue = document.querySelector(".timer-time").value;
+timerOk = document.querySelector('.timer-ok')
+
+setInterval(() => {
+    date = new Date();
+    hourValue = date.getHours();
+    minValue = date.getMinutes();
+    secondsValue = date.getSeconds();
+    if (hourValue < 10) {
+        hour.textContent = `0${hourValue} :`;
+    } else {
+        hour.textContent = ` ${hourValue} :`;
+    };
+    if (minValue < 10) {
+        min.textContent = `0${minValue} :`;
+    } else {
+        min.textContent = `${minValue} :`;
+    };
+    if (secondsValue < 10) {
+        sec.textContent = `0${secondsValue}`;
+    } else {
+        sec.textContent = secondsValue;
+    };
+});
+
+timerButton.addEventListener('click', () => {
+    timerInput.classList.toggle('timer-input_disable');
+    timerOk.classList.toggle('timer-ok');
+});
+
+const checkMouse = () => {
+    document.addEventListener('mousemove', () => {
+        document.title = "Clock";
+    })
+};
+
+timerOk.addEventListener('click', () => {
+    setTimeout(() => {
+        document.title = "Время вышло!";
+        checkMouse();
+    }, timerInputValue * 1000);
+});
